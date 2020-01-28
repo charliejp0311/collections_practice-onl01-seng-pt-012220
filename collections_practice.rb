9a6e233c7b68b41bf038fef55fecb71f0172885c
Charles Pelton <charliejp0311@gmail.com>
2020-01-28 01:21:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,7 +15,15 @@ def sort_array_desc(numbers)
 end
 
 def sort_array_char_count(words)
-
+  words.sort { |a, b|
+    if a.length == b.length
+      0
+    elsif a.length > b.length
+      1
+    elsif a.length < b.length
+      -1
+    end
+  }
 end
 
 def swap_elements(items)

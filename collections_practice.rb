c13a6f4dac1b9dd1017e27a251ad070fc52a822b
Charles Pelton <charliejp0311@gmail.com>
2020-01-28 01:11:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(numbers)
+  numbers.sort { |a, b| a <=> b }
+end 
+
+def sort_array_desc(numbers)
+
+end 
+
+def sort_array_char_count(words)
+
+end 
+
+def swap_elements(items)
+
+end 
+
+def reverse_array(numbers)
+
+end 
+
+def kesha_maker(words)
+
+end 
+
+def find_a(words) 
+
+end 
+
+def sup_array(nuumbers)
+
+end 
+
+def add_s(words)
+
+end 
 
